cf710a58bc58314cf2d3121639feba5f36f0e0a5
Nikhil <71930916+dishapatel010@users.noreply.github.com>
2024-12-11 21:48:52 +0530

    Update start.go

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -24,15 +24,11 @@ func start(ctx *ext.Context, u *ext.Update) error {
 
 	// Check if the chat type is a user
 	if peerChatId.Type != int(storage.TypeUser) {
-		log := ctx.Log.Named("start")
-		log.Info("Chat type is not user type")
 		return dispatcher.EndGroups
 	}
 
 	// Restrict access to allowed users
 	if len(config.ValueOf.AllowedUsers) != 0 && !utils.Contains(config.ValueOf.AllowedUsers, chatId) {
-		log := ctx.Log.Named("start")
-		log.Info("User not in allowed list")
 		ctx.Reply(u, "You are not allowed to use this bot.", &ext.ReplyOpts{})
 		return dispatcher.EndGroups
 	}
@@ -41,31 +37,23 @@ func start(ctx *ext.Context, u *ext.Update) error {
 	forceSubChannelID := int64(2108741045) // Replace with your actual channel ID
 	isParticipant, err := isParticipant(ctx, forceSubChannelID, chatId)
 	if err != nil {
-		log := ctx.Log.Named("start")
-		log.Info("Error checking subscription status:", err)
 		ctx.Reply(u, "An error occurred while checking your subscription status. Please try again later.", &ext.ReplyOpts{})
 		return dispatcher.EndGroups
 	}
 
 	if !isParticipant {
-		log := ctx.Log.Named("start")
-		log.Info("User is not a participant of the required channel")
 		ctx.Reply(u, "You need to subscribe to our channel to use this bot. [Subscribe here](https://t.me/your_channel_link)", &ext.ReplyOpts{})
 		return dispatcher.EndGroups
 	}
 
 	// Send welcome message if all checks pass
-	log := ctx.Log.Named("start")
-	log.Info("User is allowed and a participant. Sending welcome message.")
 	ctx.Reply(u, "Hi, send me any file to get a direct streamable link to that file.", &ext.ReplyOpts{})
 	return dispatcher.EndGroups
 }
 
 func isParticipant(ctx *ext.Context, chatID, userID int64) (bool, error) {
-	log := ctx.Log.Named("isParticipant")
 	channelPeer := ctx.PeerStorage.GetPeerById(chatID)
 	if channelPeer == nil {
-		log.Info("Channel not found")
 		return false, fmt.Errorf("channel not found")
 	}
 
@@ -77,17 +65,14 @@ func isParticipant(ctx *ext.Context, chatID, userID int64) (bool, error) {
 		Participant: ctx.PeerStorage.GetInputPeerById(userID),
 	})
 	if err != nil {
-		log.Info("Failed to get participant:", err)
 		return false, fmt.Errorf("failed to get participant: %v", err)
 	}
 
 	// Check participant type
 	switch cp.GetParticipant().(type) {
 	case *tg.ChannelParticipantLeft, *tg.ChannelParticipantBanned:
-		log.Info("User is not a participant")
 		return false, nil
 	default:
-		log.Info("User is a participant")
 		return true, nil
 	}
 }
