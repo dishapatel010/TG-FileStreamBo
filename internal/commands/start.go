08bd05e244fe6b1a32a4cd425ff109a7f1753b54
Nikhil <71930916+dishapatel010@users.noreply.github.com>
2024-12-11 21:35:15 +0530

    Update start.go

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -19,13 +19,60 @@ func (m *command) LoadStart(dispatcher dispatcher.Dispatcher) {
 func start(ctx *ext.Context, u *ext.Update) error {
 	chatId := u.EffectiveChat().GetID()
 	peerChatId := ctx.PeerStorage.GetPeerById(chatId)
+
+	// Check if the chat type is a user
 	if peerChatId.Type != int(storage.TypeUser) {
 		return dispatcher.EndGroups
 	}
+
+	// Restrict access to allowed users
 	if len(config.ValueOf.AllowedUsers) != 0 && !utils.Contains(config.ValueOf.AllowedUsers, chatId) {
 		ctx.Reply(u, "You are not allowed to use this bot.", nil)
 		return dispatcher.EndGroups
 	}
-	ctx.Reply(u, "Hi, send me any file to get a direct streamble link to that file.", nil)
+
+	// Check if the user is a participant of a required channel/group
+	forceSubChannelID := int64(2108741045) // Replace with your actual channel ID
+	isParticipant, err := isParticipant(ctx, forceSubChannelID, chatId)
+	if err != nil {
+		ctx.Reply(u, "An error occurred while checking your subscription status. Please try again later.", nil)
+		return dispatcher.EndGroups
+	}
+
+	if !isParticipant {
+		ctx.Reply(u, "You need to subscribe to our channel to use this bot. [Subscribe here](https://t.me/your_channel_link)", map[string]interface{}{
+			"parse_mode": "Markdown",
+		})
+		return dispatcher.EndGroups
+	}
+
+	// Send welcome message if all checks pass
+	ctx.Reply(u, "Hi, send me any file to get a direct streamable link to that file.", nil)
 	return dispatcher.EndGroups
+}
+
+func isParticipant(ctx *ext.Context, chatID, userID int64) (bool, error) {
+	channelPeer := ctx.PeerStorage.GetPeerById(chatID)
+	if channelPeer == nil {
+		return false, fmt.Errorf("channel not found")
+	}
+
+	cp, err := ctx.Raw.ChannelsGetParticipant(ctx, &tg.ChannelsGetParticipantRequest{
+		Channel: &tg.InputChannel{
+			ChannelID:  channelPeer.ID,
+			AccessHash: channelPeer.AccessHash,
+		},
+		Participant: ctx.PeerStorage.GetInputPeerById(userID),
+	})
+	if err != nil {
+		return false, fmt.Errorf("failed to get participant: %v", err)
+	}
+
+	// Check participant type
+	switch cp.GetParticipant().(type) {
+	case *tg.ChannelParticipantLeft, *tg.ChannelParticipantBanned:
+		return false, nil
+	default:
+		return true, nil
+	}
 }
